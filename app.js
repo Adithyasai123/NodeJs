d4e7827e34b4f96b266f129dfc25facfe14b0354
Adithyasai123 <123888728+Adithyasai123@users.noreply.github.com>
2023-07-18 19:20:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ res.send("<h2>Welcome to Node JS express application</h2>"+appEnv.url+appEnv.por
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/adithya', function(request, response) {
+app.get('/mithuntechnologies', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mabp. Please Contact +91-9491785919 for more information or send an email to adithyaacgacg3@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
     response.end();
     
   })
@@ -44,10 +44,10 @@ app.get("/html", function(req,res){
         //res.type('application/json');
         //res.json({'name': 'Mithun Reddy L'});
         res.send({
-		'name': 'adithya',
+		'name': 'Mithun Technologies',
 		'technology': 'DevOps',
-		'contact' : '9491785919',
-		'email': 'adithyaacgacg3@gmail.com'
+		'contact' : '9980923226',
+		'email': 'devopstrainingblr@gmail.com'
 	            });
         
         });
@@ -75,10 +75,6 @@ app.get('/redirect', function(req, res) {
     
  
     app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/adithya");
+        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
       })
     
-
-
-
-
